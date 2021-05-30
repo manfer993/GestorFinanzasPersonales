24fc61a29ea457d90b863eca31162d28f7c15bc3
Manuel Munoz <manfer93@gmail.com>
2021-05-29 22:18:08 -0500

    add service usuario

---
+++ src/app/service/usuario.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import User from '../models/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  getUser(user: User) {
+    const loginUser = { user:user.email, pass:user.password }
+    return this.http.get('http://localhost:80/rest/src/usuarios.php',{params: loginUser});
+  }
+}
